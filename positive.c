10e1f7b52db50aef382a1e461cad939d232c68c5
mohanrajvsb <mohanckpc@gmail.com>
2017-06-20 17:01:23 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,24 @@
+
+ 
+#include <stdio.h>
+#$include<conio.h>
+ 
+Void main()
+{
+    int num;
+     printf("Enter any number: ");
+    scanf("%d", &num);
+     if(num > 0)
+    {
+        printf("Number is +ve");
+    }
+    else if(num < 0)
+    {
+        printf("Number is -ve");
+    }
+    else
+    {
+        printf("Number is 0");
+    }
+ 
+    }
