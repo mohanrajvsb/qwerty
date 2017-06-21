2c8326165f246e1f6159be21065129f0beeddaf5
mohanrajvsb <mohanckpc@gmail.com>
2017-06-21 12:12:44 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    double num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &num1, &num2, &num3);
+
+    if( num1>=n2 && num1>=n3 )
+        printf("%.2f is the largest number.", num1);
+
+    if( num2>=num1 && num2>=num3 )
+        printf("%.2f is the largest number.", num2);
+
+    if( num>=num1 && num3>=num2 )
+        printf("%.2f is the largest number.", num3);
+    }
