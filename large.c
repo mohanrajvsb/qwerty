1ff50b0f3e1b53db9140f0e4d2e3ffaab12843dd
mohanrajvsb <mohanckpc@gmail.com>
2017-06-21 12:13:32 +0530

    Update large.c

--- large.c
+++ large.c
@@ -5,7 +5,7 @@ void main()
     printf("Enter three numbers: ");
     scanf("%lf %lf %lf", &num1, &num2, &num3);
 
-    if( num1>=n2 && num1>=n3 )
+    if( num1>=num2 && num1>=n3 )
         printf("%.2f is the largest number.", num1);
 
     if( num2>=num1 && num2>=num3 )
