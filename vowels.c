058bc1c075884fd10bfb12de8e1ab14ca2872412
mohanrajvsb <mohanckpc@gmail.com>
2017-06-21 08:54:55 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -2,10 +2,10 @@
  void main()
 {
   char ch;
-   printf("Enter a character\n");
+   printf("Enter a ch\n");
   scanf("%c", &ch);
    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
-    printf("%c is a vowel.\n", ch);
+    printf("%c is a vow.\n", ch);
   else
-    printf("%c is not a vowel.\n", ch);
+    printf("%c is not a vow\n", ch);
  }
