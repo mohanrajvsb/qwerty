f30c16bae7d92b3aa332d8e44bcac0c0970c4134
mohanrajvsb <mohanckpc@gmail.com>
2017-07-01 16:04:33 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int n, i, s = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=1; i <= n; ++i)
+    {
+        s= s+i;  
+    }
+    printf("Sum = %d",s);
+}
