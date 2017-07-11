ae7a8b2bed95c01b5bd98e1b1a674794e6eda3e5
mohanrajvsb <mohanckpc@gmail.com>
2017-07-11 06:30:33 -0700

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main()
 {
-    int n, i,  = 0;
+    int n, i,  f= 0;
     printf("Enter  integer: ");
     scanf("%d",&n);
     for(i=2; i<=n/2; ++i)
     {
        f(n%i==0)
         {
-            flag=1;
+            f=1;
             break;
         }
     }
 
-    if (flag==0)
+    if (f==0)
         printf("%d is a prime no.",n);
     else
         printf("%d is not a prime no.",n);
