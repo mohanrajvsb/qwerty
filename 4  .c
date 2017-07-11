962dfc865332bf3d004d09dc75e5a7ebced2a501
mohanrajvsb <mohanckpc@gmail.com>
2017-07-11 07:44:19 -0700

    Update and rename 4 .c to 4  .c

--- 4  .c
+++ 4  .c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void main()
 
-{    int n1,m =20,n=40;
+{    int n1,m =20,n=30;
     clrscr();
-    printf("Print Odd Numbers in a given range m to n:\n");
+    printf("Print Odd Numbers  m to n:\n");
     for (num = m; n1 <= n; num++)
         {
             if (n % 2 == 1)
