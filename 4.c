97b4336c4fbd8edfdcf3350aacbc33857a54d18e
mohanrajvsb <mohanckpc@gmail.com>
2017-07-11 06:34:06 -0700

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+
+{    int n1,m =20,n=40;
+    clrscr();
+    printf("Print Odd Numbers in a given range m to n:\n");
+    for (num = m; n1 <= n; num++)
+        {
+            if (n % 2 == 1)
+              printf ("%d ", n1);
+        }
+   getch();
+
+}
