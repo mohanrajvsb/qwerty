9287ac6bf93e484320b46c94df493d08602e708e
mohanrajvsb <mohanckpc@gmail.com>
2017-07-11 07:47:50 -0700

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+   
+int main() {  
+    int c; 
+    printf("Even numbers between 1 to 10\n");  
+        for(counter = 1; counter <= 10; c++) {  
+         if(c%2 == 0) { 
+          printf("%d ", c);  
+        }  
+    }  
+   
+    return 0;  
+} 
