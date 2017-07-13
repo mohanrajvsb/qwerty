7cc94ad812f2e77c530586f59b437525736e8ed0
mohanrajvsb <mohanckpc@gmail.com>
2017-07-13 06:53:56 -0700

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    factorial = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+      for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              
+        }
+        printf("Factorial of %d ",  factorial);
+ }
+
+   
