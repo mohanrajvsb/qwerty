fbbb158dc0d9dfe523629d27ac126e7ed81e90e7
mohanrajvsb <mohanckpc@gmail.com>
2017-07-15 03:49:21 -0700

    Update and rename s2.c to s 2.c

--- s 2.c
+++ s 2.c
@@ -2,7 +2,7 @@
 int main()
 {
     int n1, n2, i, gcd;
-    printf("Enter two integers: ");
+    printf("Enter two int: ");
     scanf("%d %d", &n1, &n2);
     for(i=1; i <= n1 && i <= n2; ++i)
     {
